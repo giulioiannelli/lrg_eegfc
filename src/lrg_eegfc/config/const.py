c3e339ec738cfa40b0902202a3f4f43c77b22f47
giulioiannelli <giulioiannelli.w@gmail.com>
2025-05-13 18:19:29 +0200

    Add configuration constants for brain wave bands and update core module imports

---
+++ src/lrg_eegfc/config/const.py
@@ -0,0 +1,8 @@
+BRAIN_BANDS = {
+    'delta':      (0.53,   4),
+    'theta':      (4,      8),
+    'alpha':      (8,     13),
+    'beta':       (13,    30),
+    'low_gamma':  (30,    80),
+    'high_gamma': (80,   300),
+}
--- src/lrg_eegfc/core.py
+++ src/lrg_eegfc/core.py
@@ -1,2 +1,3 @@
 from .shared import *
+from .config import *
 from .utils import *
