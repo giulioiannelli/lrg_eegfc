cf8555d37131dbb241cf3612f85ae10b6cfea7b8
giulioiannelli <giulioiannelli.w@gmail.com>
2025-05-29 12:50:47 +0200

    Add BRAIN_BANDS_LABELS to define labels for brain band frequencies

--- src/lrg_eegfc/config/const.py
+++ src/lrg_eegfc/config/const.py
@@ -6,6 +6,14 @@ BRAIN_BANDS = {
     'low_gamma':  (30,    80),
     'high_gamma': (80,   300),
 }
+BRAIN_BANDS_LABELS = {
+    'delta':      r'$\delta$',
+    'theta':      r'$\theta$',
+    'alpha':      r'$\alpha$',
+    'beta':       r'$\beta$',
+    'low_gamma':  r'$\gamma_{\rm l}$',
+    'high_gamma': r'$\gamma_{\rm h}$',
+}
 
 phase_labels = ['rsPre', 'taskLearn', 'taskTest', 'rsPost']
 param_keys_list = ['fs', 'fcutHigh', 'fcutLow', 'filter_order', 'NotchFilter', 'DataDimensions']
