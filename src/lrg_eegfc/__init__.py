d3db8ebf42becaf04b10ef4a551d00a53948cb37
giulioiannelli <giulioiannelli.w@gmail.com>
2025-06-11 14:11:56 +0200

    Add const module and update __init__.py for imports

--- src/lrg_eegfc/__init__.py
+++ src/lrg_eegfc/__init__.py
@@ -1,1 +1,4 @@
 from .core import *
+from .const import *
+from .config.ipy import *
+from .config.plotlib import *
--- src/lrg_eegfc/shared.py
+++ src/lrg_eegfc/shared.py
@@ -1,6 +1,11 @@
 import emd
 import h5py
+import logging
 import os
 
 import numpy as np
 import pandas as pd
+
+from pathlib import Path
+from typing import List, Dict, Any, Tuple
+from scipy.io import loadmat
